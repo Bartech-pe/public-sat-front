4c978a83d92f1349e3f7ae8c9e9e6cbb5d4b479f
Arkel <144744565+WalkedPlate@users.noreply.github.com>
2025-10-28 16:17:38 -0500

    UPDATE 28/10 - 1

--- src/app/features/main/campaigns/manage-campaign/multi-campaign-audio/multi-campaign-audio.component.ts
+++ src/app/features/main/campaigns/manage-campaign/multi-campaign-audio/multi-campaign-audio.component.ts
@@ -16,6 +16,7 @@ import * as XLSX from 'xlsx';
 import { FileUploadModule } from 'primeng/fileupload';
 import { InputTextModule } from 'primeng/inputtext';
 import { MessageGlobalService } from '@services/generic/message-global.service';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 
 
 @Component({
@@ -54,14 +55,18 @@ export class MultiCampaignAudioComponent implements OnInit{
   excelHeaders: string[] = [];  // Headers extraídos del Excel
   excelData: any[] = [];  // Datos para preview (primer fila mock)
   loading = false;
-  previewData: { [key: string]: string } = {};  // Para preview del mensaje
+  previewData: any[] = [];
+
+  campaign:any;
   
   readonly departmentStore = inject(DepartmentStore);
   get departmentList(): Department[] {
       return this.departmentStore.items();
   }
 
-   private readonly msg = inject(MessageGlobalService);
+  private readonly msg = inject(MessageGlobalService);
+  
+   public readonly ref: DynamicDialogRef = inject(DynamicDialogRef);
   constructor(
     
     private vicidialService: VicidialService, 
@@ -94,17 +99,20 @@ export class MultiCampaignAudioComponent implements OnInit{
       const workbook = XLSX.read(data, { type: 'array' });
       const sheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[sheetName];
-      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { defval: '' });
+      const jsonDataHeader = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+      if (jsonData.length === 0) {
+          this.msg.error('El archivo está vacío');
+          return;
+      }
 
       // Extrae headers (primera fila)
-    const rawHeaders = jsonData[0];
+    const rawHeaders = jsonDataHeader[0];
     this.excelHeaders = Array.isArray(rawHeaders) ? (rawHeaders as string[]) : [];
-
-     
-      this.excelData = jsonData.slice(1);
-      this.previewData = this.excelData[0] || {};  // Usa primera fila para preview
-
-      this.loading = false;
+    console.log(this.excelHeaders)
+    this.previewData = jsonData;
+    this.loading = false;
      
     };
     reader.readAsArrayBuffer(file);
@@ -139,51 +147,79 @@ export class MultiCampaignAudioComponent implements OnInit{
     this.updatePreview();
   }
 
-  insertVariableIphone(event: any){
-      const variableIphone = event.value;
-
-      if (!variableIphone) return;
-
-      // Validar que sea numérico
-      const esNumerico = /^[0-9]+$/.test(variableIphone);
-
-      // Validar longitud máxima de 9 dígitos
-      const longitudValida = variableIphone.toString().length <= 9;
-
-      if (!esNumerico) {
-  
-        this.msg.warn('El valor debe ser numérico');
-        this.contact = '';
-        return;
-      }
-
-      if (!longitudValida) {
-        this.msg.warn('El número no puede tener más de 9 dígitos');
-        this.contact = '';
-        return;
-      }
-  }
-
-  // Preview del mensaje con variables resueltas
   updatePreview() {
     let preview = this.messageText;
-    this.excelHeaders.forEach(header => {
-      preview = preview.replace(new RegExp(`{{${header}}}`, 'g'), this.previewData[header] || '[Sin dato]');
-    });
-    //this.previewData.messagePreview = preview;  // Para binding en HTML
+    // this.excelHeaders.forEach(header => {
+    //   preview = preview.replace(new RegExp(`{{${header}}}`, 'g'), this.previewData[header] || '[Sin dato]');
+    // });
   }
 
-  // Submit form (mock: envía a API)
+  private renderTemplate(template: string, contacto: any): string {
+    // Buscar todas las variables entre corchetes como [NOMBRE], [CORREO], [CELULAR], etc.
+    return template.replace(/\[([^\]]+)\]/g, (_, variable) => {
+      const key = variable.trim().toUpperCase();
+      // Si el contacto tiene una propiedad con ese nombre, la reemplaza, si no deja vacío
+      return contacto[key] ?? '';
+    });
+  }
+
+
   submitForm() {
-    if (!this.selectedCampaign || this.excelHeaders.length === 0 || !this.selectedArea || !this.listId || !this.name || !this.description) {
-     
+
+     if (!this.selectedCampaign || this.excelHeaders.length === 0 || !this.selectedArea || !this.listId || !this.name || !this.description) {
+      this.msg.warn("AL CAMPOS REQUERIDOS")
       return;
     }
 
+    let request = {
+        list_id: this.listId,
+        list_name: this.name,
+        list_description: this.description,
+        campaign_id: this.selectedCampaign,
+        active: 'N',
+        departmentId:this.selectedArea,
+        campaign_name:this.campaign?.campaign_name,
+    }
 
+
+    const campaignEmails = this.previewData.map((contacto: any) => ({
+       script_text: this.renderTemplate(this.messageText, contacto),
+       phone_number:contacto[this.contact], 
+       vicidial_lead_id: this.listId,
+    }));
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(campaignEmails);
+    const workbook: XLSX.WorkBook = {
+      Sheets: { CampaignEmails: worksheet },
+      SheetNames: ['CampaignEmails'],
+    };
+ 
+ 
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    const fileName = `campaign_emails_${new Date().getTime()}.xlsx`;
+    const file = new File([excelBuffer], fileName, {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+
+
+    this.vicidialService.createlistaMultiple(request, file).subscribe({
+         next: (res) => {
+             this.msg.success('Leads guardados correctamente');
+             this.onCancel();
+         },
+         error: (err) => {},
+       });
   }
 
-  onCancel(){
-
+  onCampaignChange(event: any) {
+      const selectedCampaingId = event.value;
+      if(selectedCampaingId){
+          this.campaign = this.listCampaignVicidial.find(
+            (res: any) => res.campaign_id == selectedCampaingId
+          );
+      }
   }
+
+  onCancel(){this.ref.close();}
 }
